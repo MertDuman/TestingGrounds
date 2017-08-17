03c64fa154256f8e6add91b59a6d4e7bd2442ea2
Mert Duman <duman.m.mert@gmail.com>
2017-08-17 17:13:05 +0300

    Added Gun C++ class. Now Gun is an Actor that will be added to Characters. Cleared out all Gun and VR related stuff from FirstPersonCharacter. Modified the projectile to be smaller and faster. (Need a new shooting system)

---
+++ Source/TestingGrounds/Private/Gun.cpp
@@ -0,0 +1,68 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Gun.h"
+#include "TestingGroundsProjectile.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Animation/AnimInstance.h"
+
+
+// Sets default values
+AGun::AGun()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Create a gun mesh component
+	FP_Gun = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FP_Gun"));
+	FP_Gun->bCastDynamicShadow = false;
+	FP_Gun->CastShadow = false;
+	FP_Gun->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void AGun::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGun::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AGun::OnFire() {
+	// try and fire a projectile
+	if (ProjectileClass != NULL) {
+		UWorld* const World = GetWorld();
+		if (World != NULL) {
+			// Get the spawn transforms
+			const FRotator SpawnRotation = FP_Gun->GetSocketRotation(FName("Muzzle"));
+			const FVector SpawnLocation = FP_Gun->GetSocketLocation(FName("Muzzle"));
+
+			//Set Spawn Collision Handling Override
+			FActorSpawnParameters ActorSpawnParams;
+			ActorSpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButDontSpawnIfColliding;
+
+			// Spawn the projectile at the muzzle
+			World->SpawnActor<ATestingGroundsProjectile>(ProjectileClass, SpawnLocation, SpawnRotation, ActorSpawnParams);
+		}
+	}
+
+	// try and play the sound if specified
+	if (FireSound != NULL) {
+		UGameplayStatics::PlaySoundAtLocation(this, FireSound, GetActorLocation());
+	}
+
+	// try and play a firing animation if specified
+	if (FireAnimation != NULL) {
+		// Get the animation object for the arms mesh
+		if (AnimInstance != NULL) {
+			AnimInstance->Montage_Play(FireAnimation, 1.f);
+		}
+	}
+}
+
