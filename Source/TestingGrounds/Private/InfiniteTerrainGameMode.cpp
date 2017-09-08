22c17bae32dc49908c385a9d20180226786caeeb
Mert Duman <duman.m.mert@gmail.com>
2017-09-08 17:22:02 +0300

    AI now uses EQS to patrol in the Tile. Started creating pools to use NavMesh on generated Tiles.

---
+++ Source/TestingGrounds/Private/InfiniteTerrainGameMode.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "InfiniteTerrainGameMode.h"
+#include "EngineUtils.h"
+
+void AInfiniteTerrainGameMode::FindAllNavMeshBoundsVolumes() {
+}
+
+void AInfiniteTerrainGameMode::AddToPool(ANavMeshBoundsVolume * VolumeToAdd) {
+}
+
+
